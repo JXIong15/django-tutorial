112bc7617e545827e2fe2646322329c24e752c40
Jou Xiong <jouxiong15@gmail.com>
2021-09-14 20:07:37 -0500

    finished video walkthrough

--- playground/views.py
+++ playground/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def calculate():
+    x=1
+    y=2
+    return x
+
 def say_hello(request):
-    x = 1
-    y = 2
+    x = calculate()
     return render(request, 'hello.html', {'name': 'Mosh'})
